05ca8c2a87e6f127ea6d0b2fc076ec65322f7393
Muhammad Ramadhan <mramadhan742@gmail.com>
2021-09-22 16:23:24 +0700

    Added New Project
    Chapter 5 - Angry Bird

---
+++ Assets/Script/YellowBird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBird : Bird
+{
+    [SerializeField]
+    public float boostForce;
+    public bool hasBoost = false;
+
+    public void Boost()
+    {
+        if (State == BirdState.Thrown && !hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * boostForce);
+            hasBoost = true;
+        }
+    }
+
+    public override void OnTap()
+    {
+        Boost();
+    }
+    
+    
+}
